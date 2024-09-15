7d0644d5d6de14131955bb8b9c0ce71ca1f44e71
Diego <verasdiegoca78@gmail.com>
2024-09-14 22:43:30 -0300

    Add Vercel configuration for API rewrites

--- react-app/src/services/teacher.js
+++ react-app/src/services/teacher.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const teacherService = axios.create({
-  baseURL: "http://34.203.199.235:8080/evaluations", // URL base del backend
+  baseURL: "https://34.203.199.235:8080/evaluations", // URL base del backend
   headers: {
     "Content-Type": "application/json",
   },
